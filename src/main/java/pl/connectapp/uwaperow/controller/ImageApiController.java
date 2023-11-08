158afd74486cfa20a608d8e4701c6dfe289639ba
mikolajdudek <mikidudek12@gmail.com>
2023-11-08 18:06:07 +0100

    update(working all)

--- src/main/java/pl/connectapp/uwaperow/controller/ImageApiController.java
+++ src/main/java/pl/connectapp/uwaperow/controller/ImageApiController.java
@@ -1,23 +1,17 @@
 package pl.connectapp.uwaperow.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.connectapp.uwaperow.model.Image;
 import pl.connectapp.uwaperow.model.dto.ImageDTO;
 import pl.connectapp.uwaperow.repository.ImageRepository;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,14 +23,17 @@ public class ImageApiController {
     @Autowired
     private ImageRepository imageRepository;
 
-    @GetMapping("/getAllFromRepo")
-    public ResponseEntity<List<ImageDTO>> getAllProducts() {
+    @GetMapping("/getImagesByKeyword")
+    public ResponseEntity<List<ImageDTO>> getImagesByKeyword(@RequestParam("keyword") String keyword) {
         List<Image> images = imageRepository.findAll();
 
         List<ImageDTO> imageDTOs = images.stream()
+                .filter(image -> image.getImageUrl().contains(keyword)) // Filtrowanie po zawartości URL
                 .map(image -> new ImageDTO(image.getImageUrl()))
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(imageDTOs);
     }
 }
+
+
